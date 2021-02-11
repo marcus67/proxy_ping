99883cef3bb3cda73a727c94e22b5aad4b1fa930
Marcus Rickert <marcus.rickert@web.de>
2021-02-12 00:46:18 +0100

    *  Add logo *  Correct some CI web links

---
+++ CHANGES.md
@@ -0,0 +1,9 @@
+![ProxyPingLogo](doc/proxy_ping_logo_256x256.png)
+
+# Change History 
+
+This document lists all changes of `ProxyPing` with the most recent changes at the top.
+
+## Version 0.1.1 Revision 1 (February 6th, 2021)
+
+*   Initial version
--- setup.py
+++ setup.py
@@ -57,26 +57,24 @@ extended_setup_params = {
     #"deploy_sudoers_file": True,
     #"deploy_apparmor_file": True,
     "contributing_setups": ["python_base_app", "some_flask_helpers"],
-    "publish_debian_package": ["master", "release"],
-    "publish_docker_images": ["master", "release"],
+    "publish_debian_package": ["main", "release"],
+    #"publish_docker_images": ["main", "release"],
     #"publish_latest_docker_image": "release",
-    #"debian_extra_files": [
-    #    ("etc/slave.config", "etc/proxy-ping/slave.config"),
-    #    ("etc/master.config", "etc/proxy-ping/master.config"),
-    #],
+    "debian_extra_files": [
+        ("etc/proxy-ping.template.config", "etc/proxy-ping/proxy-ping.template.config"),
+    ],
     "debian_templates": [
-        ("/etc/proxy-ping/proxy-ping.config", "/etc/proxy-ping/proxy-ping.config")
+        ("/etc/proxy-ping/proxy-ping.template.config", "/etc/proxy-ping/proxy-ping.config")
     ],
     #"build_pypi_package": True,
     #"publish_pypi_package": { 'release': ( 'https://upload.pypi.org/legacy/', 'PYPI_API_TOKEN' ),
-    #                          'master': ( 'https://test.pypi.org/legacy/', 'TEST_PYPI_API_TOKEN') },
+    #                          'main': ( 'https://test.pypi.org/legacy/', 'TEST_PYPI_API_TOKEN') },
     #"generate_generic_install": True,
 }
 
 setup_params.update(proxy_ping.settings.settings)
 extended_setup_params.update(proxy_ping.settings.extended_settings)
 extended_setup_params.update(setup_params)
-
 
 if __name__ == '__main__':
     setup(**setup_params)
